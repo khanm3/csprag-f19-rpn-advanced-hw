02f133d61e4207a997ebc2c96613aca8dbea9272
Mohammad Khan <khanmo2k@gmail.com>
2019-11-03 12:38:02 -0500

    Implement subtract

--- rpn.py
+++ rpn.py
@@ -4,9 +4,14 @@ def calculate(arg):
     stack = list()
     for token in arg.split():
         if token == '+':
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = arg1 + arg2
+            stack.append(result)
+        elif token == '-':
             arg1 = stack.pop()
             arg2 = stack.pop()
-            result = arg1 + arg2
+            result = arg2 - arg1
             stack.append(result)
         else:
             stack.append(int(token))
