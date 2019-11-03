2342597ac59b7bb66b0321aeab9480dafc665cda
Mohammad Khan <khanmo2k@gmail.com>
2019-11-03 15:17:20 -0500

    Add tests for division
    There are two types of results when using integer division. One is
    where the division is even (no remainder), and the other uses floor
    to eliminate remainders (as in floating point values).

--- test_rpn.py
+++ test_rpn.py
@@ -11,6 +11,12 @@ class TestBasics(unittest.TestCase):
     def test_mul(self):
         result = rpn.calculate('4 8 *')
         self.assertEqual(32, result)
+    def test_div1(self):
+        result = rpn.calculate('5 3 /')
+        self.assertEqual(1, result)
+    def test_div2(self):
+        result = rpn.calculate('12 3 /')
+        self.assertEqual(4, result)
     def test_badinput(self):
         with self.assertRaises(TypeError):
             rpn.calculate('1 2 3 +')
