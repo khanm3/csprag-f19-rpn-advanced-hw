25206ae442d2fab86eff895f7b1c39a6438d6cd5
Mohammad Khan <khanmo2k@gmail.com>
2019-11-03 15:06:12 -0500

    Reduce copied code from add and subtract

--- rpn.py
+++ rpn.py
@@ -1,20 +1,29 @@
 #!/usr/bin/env python3
 
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+operators = {
+    '+': add,
+    '-': sub,
+}
+
 def calculate(arg):
     stack = list()
     for token in arg.split():
-        if token == '+':
+        try:
+            value = int(token)
+            stack.append(value)
+        except ValueError:
+            function = operators[token]
             arg2 = stack.pop()
             arg1 = stack.pop()
-            result = arg1 + arg2
+            result = function(arg1, arg2)
             stack.append(result)
-        elif token == '-':
-            arg1 = stack.pop()
-            arg2 = stack.pop()
-            result = arg2 - arg1
-            stack.append(result)
-        else:
-            stack.append(int(token))
+
         #print(stack)
     if len(stack) != 1:
         raise TypeError('malformed input')
